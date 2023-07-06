56f63631f66dc97557b70c71f43e8296ca7f6573
camilleramos1 <129894673+camilleramos1@users.noreply.github.com>
2023-07-06 13:58:18 -0600

    fixed delete button

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,38 +5,37 @@ const path = require('path');
 // using uuid for unique id for each note
 const uuid = require('uuid');
 
-// creating a const for the JSON parse
-const notes = JSON.parse(fs.readFileSync('db/db.json'));
 
 // using this for routing
 module.exports = (app) => {
     // get route for api/notes
     app.get('/api/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, 'db/db.json'));
+        let notes = JSON.parse(fs.readFileSync('./db/db.json'));
+        res.json(notes);
     });
     // post request for api/notes using JSON including err catching
     app.post('/api/notes', (req, res) => {
+        let notes = JSON.parse(fs.readFileSync('./db/db.json'));
         const newNote = {
             title: req.body.title,
             text: req.body.text,
         };
-        newNote.id = uuid();
+        newNote.id = uuid.v4();
         notes.push(newNote);
         const noteString = JSON.stringify(notes);
-        fs.writeFile('db/db.json', noteString, err => {
+        fs.writeFile('./db/db.json', noteString, err => {
         if (err) throw err;
         res.json(notes);
         });
     });
     // delete notes using id
     app.delete('/api/notes/:id', (req, res) => {
-        console.log("Notes: ",notes);
+        let notes = JSON.parse(fs.readFileSync('./db/db.json'));
+        // console.log("Notes: ",notes);
         const deleteNote = notes.filter((note) => note.id !== req.params.id);
-        const noteString = JSON.stringify(deleteNote);
-        fs.writeFileSync('db/db.json', JSON.stringify(deleteNote), err => {
-        res.json(err);
+        fs.writeFile('./db/db.json', JSON.stringify(deleteNote), 'utf-8', (err) => {
+        if (err) throw err;
+        res.json(deleteNote);
         });
-        res.json(notes);
-        // should this be res.json(deleteNote)??
     });
 };
